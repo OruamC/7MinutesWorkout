ade103171ad202142eda073b506ef3e296fa2a86
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-10-28 17:41:05 -0300

    Adding upcoming exercise to rest view

--- app/src/main/java/com/oruam/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/oruam/a7minutesworkout/ExerciseActivity.kt
@@ -47,6 +47,9 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvExerciseName?.visibility = View.INVISIBLE
         binding?.flExerciseView?.visibility = View.INVISIBLE
         binding?.ivImage?.visibility = View.INVISIBLE
+        binding?.tvUpcomingLabel?.visibility = View.VISIBLE
+        binding?.tvUpComingExerciseName?.visibility = View.VISIBLE
+        binding?.tvUpComingExerciseName?.text = exerciseList!![currentExercisePosition + 1].name
 
         if (restTimer != null) {
             restTimer?.cancel()
@@ -61,6 +64,8 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.tvExerciseName?.visibility = View.VISIBLE
         binding?.flExerciseView?.visibility = View.VISIBLE
         binding?.ivImage?.visibility = View.VISIBLE
+        binding?.tvUpcomingLabel?.visibility = View.INVISIBLE
+        binding?.tvUpComingExerciseName?.visibility = View.INVISIBLE
 
         if (exerciseTimer != null) {
             exerciseTimer?.cancel()
