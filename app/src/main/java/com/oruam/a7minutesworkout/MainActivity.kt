5cac4ee0282e6e2898fa83198336389d45c9732c
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-10-28 11:26:51 -0300

    Using view binding on main activity

--- app/src/main/java/com/oruam/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/oruam/a7minutesworkout/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.oruam.a7minutesworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.oruam.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton : FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener {
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(
                 this@MainActivity,
                 "Here we will start the exercise",
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
