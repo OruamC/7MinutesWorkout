f11311542379044aab973686ddbe437dc848079d
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-10-28 15:14:53 -0300

    Changing between rest and exercise timer

--- app/src/main/java/com/oruam/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/oruam/a7minutesworkout/ExerciseActivity.kt
@@ -3,6 +3,7 @@ package com.oruam.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.oruam.a7minutesworkout.databinding.ActivityExerciseBinding
 
@@ -11,6 +12,9 @@ class ExerciseActivity : AppCompatActivity() {
 
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,16 +44,16 @@ class ExerciseActivity : AppCompatActivity() {
         setRestProgressBar()
     }
 
-    // necessary implements this methods to avoid memory leak, because of the view binding
-    override fun onDestroy() {
-        super.onDestroy()
+    private fun setupExerciseView() {
+        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.tvTitle?.text = "Exercise Name"
+        binding?.flExerciseView?.visibility = View.VISIBLE
 
-        if (restTimer != null) {
-            restTimer?.cancel()
-            restProgress = 0
+        if(exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
         }
-
-        binding = null
+        setExerciseProgressBar()
     }
 
     private fun setRestProgressBar() {
@@ -63,12 +67,45 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setExerciseProgressBar() {
+        binding?.progressBarExercise?.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
                 Toast.makeText(
                     this@ExerciseActivity,
-                    "Here now we will start the exercise.",
+                    "30 seconds are over, lets go to the rest",
                     Toast.LENGTH_SHORT
                 ).show()
             }
         }.start()
+    }
+
+    // necessary implements this methods to avoid memory leak, because of the view binding
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        if(exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        binding = null
     }
 }
