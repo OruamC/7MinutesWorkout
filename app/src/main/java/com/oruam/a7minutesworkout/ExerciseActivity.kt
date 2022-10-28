d0d888be20e7ff0d55c9bdfa31b1188600ba9207
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-10-28 14:55:24 -0300

    Get count down progress bar working

--- app/src/main/java/com/oruam/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/oruam/a7minutesworkout/ExerciseActivity.kt
@@ -2,10 +2,15 @@ package com.oruam.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.oruam.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     var binding: ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +20,7 @@ class ExerciseActivity : AppCompatActivity() {
         setSupportActionBar(binding?.toolbarExercise)
 
         // Generates de back button on the toolbar
-        if(supportActionBar != null) {
+        if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
 
@@ -23,12 +28,47 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    private fun setupRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
     }
 
     // necessary implements this methods to avoid memory leak, because of the view binding
     override fun onDestroy() {
         super.onDestroy()
 
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
         binding = null
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
     }
 }
