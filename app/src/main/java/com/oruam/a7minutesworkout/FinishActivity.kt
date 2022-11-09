a4cfb73826235f4eaf37ebfb1279a9252f725762
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-11-09 11:40:44 -0300

    Room setup and workout date insert

--- app/src/main/java/com/oruam/a7minutesworkout/FinishActivity.kt
+++ app/src/main/java/com/oruam/a7minutesworkout/FinishActivity.kt
@@ -1,8 +1,13 @@
 package com.oruam.a7minutesworkout
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.oruam.a7minutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -28,5 +33,22 @@ class FinishActivity : AppCompatActivity() {
             finish()
         }
 
+        val historyDao = (application as WorkOutApp).db.historyDao()
+        addDateToDatabase(historyDao)
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao) {
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        Log.e("Date", "" + dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.e("Formatted Date", "" + date)
+
+        lifecycleScope.launch {
+            historyDao.insert(HistoryEntity(date))
+            Log.e("Date : ", "Added...")
+        }
     }
 }
